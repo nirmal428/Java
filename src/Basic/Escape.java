1c02150334442445c3328964d3cd65fb5cb6c450
nirmal428 <mauryanirmal428@gmail.com>
2025-08-02 00:35:29 +0530

    init commit

--- src/Basic/Escape.java
+++ src/Basic/Escape.java
@@ -4,6 +4,6 @@ public class Escape {
     public static void main(String[] args) {
         System.out.println("Hello \n\"Guys\"....." );
 
-        System.out.print("HELLO \\ \bBROTHER \\");
+        System.out.print("HELLO \\\b BROTHER \\");
     }
 }
