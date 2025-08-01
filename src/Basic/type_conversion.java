c0f5665e81a112100824117a535fdecd1ea2b22b
nirmal428 <mauryanirmal428@gmail.com>
2025-08-02 01:10:01 +0530

    init commit

---
+++ src/Basic/type_conversion.java
@@ -0,0 +1,11 @@
+package Basic;
+
+public class type_conversion{
+    public static void main(String[] args) {
+        float ft=5;
+        System.out.println(ft);
+
+        int myint =(int) 5.0f;
+        System.out.println(myint);
+    }
+}
