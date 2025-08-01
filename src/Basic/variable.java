49720c8f8bf52da5e60bc6f63b08fbc2038c0d1c
nirmal428 <mauryanirmal428@gmail.com>
2025-08-02 00:16:13 +0530

    init commit

---
+++ src/Basic/variable.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class variable {
+    public static void main(String[] args) {
+        int myNum= 7;
+
+        int yourNum;
+
+        yourNum=7465;
+
+        System.out.println(yourNum);
+        yourNum=17;
+        System.out.println(yourNum);
+    }
+}
