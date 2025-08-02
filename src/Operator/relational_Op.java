e423d6fe23d50941309639615c1b94160764d715
nirmal428 <mauryanirmal428@gmail.com>
2025-08-02 22:27:34 +0530

    init commit

---
+++ src/Operator/relational_Op.java
@@ -0,0 +1,27 @@
+package Operator;
+
+public class relational_Op {
+    public static void main(String[] args) {
+        int a=10;
+        int d=20;
+        int c=10;
+        if(a >= d){
+            System.out.println(" a is greater than d");
+        }
+        else{
+            System.out.println("a is less than d ");
+        }
+        if(a == c){
+            System.out.println(" a is equal to c");
+        }
+        else{
+            System.out.println("a is notequal to c ");
+        }
+        if(a != d){
+            System.out.println(" a is not equal to d");
+        }
+        else{
+            System.out.println("a is equal to d ");
+        }
+    }
+}
