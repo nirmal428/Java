e1831bd229adae611880001ee7671fa829f81ddc
nirmal428 <mauryanirmal428@gmail.com>
2025-08-05 03:59:03 +0530

    init commit

---
+++ src/Basic/Return.java
@@ -0,0 +1,22 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Return {
+    public static void main(String[] args) {
+        greet();
+        int a= input();
+        int b= input();
+        int sum =a+b;
+        System.out.print("Sum od a and b = "+ sum);
+    }
+    public static void greet(){
+        System.out.println("Perform Addition Operation ");
+    }
+    public static int input(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your first number : ");
+        int number= sc.nextInt();
+        return number;
+    }
+}
